e4740750dbc511db5921cdd62efbf2ed0e0f3b52
mattdogg1929 <ezekiel_yeager@protonmail.com>
2021-03-30 19:44:11 -0500

    first draft of the token contract

--- contracts/HalpCoin.sol
+++ contracts/HalpCoin.sol
@@ -6,25 +6,61 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
  
 contract HalpCoin is IERC20Upgradeable, Initializable {
+
   uint256 _totalSupply;
+  string private _name;
+  string private _symbol;
+
+  mapping (address => uint256) private _balances;
+  //mapping (address => mapping (address => uint256)) private _allowances;
 
   function initialize() initializer public {
+    _name = 'HalpCoin';
+    _symbol = 'HALP';
     _totalSupply = 10000000;
   }
 
+  function name() public view returns (string memory) {
+    return _name;
+  } 
+
+  function symbol() public view returns (string memory) {
+    return _symbol;
+  }
+
+  //overriding the erc20 spec
   function totalSupply() public view override returns (uint256) {
     return _totalSupply;
   }
 
-  function balanceOf(address account) public pure override returns (uint256) {
-    return 0;
+  function balanceOf(address account) public view virtual override returns (uint256) {
+    return _balances[account];
   }
 
-  function transfer(address recipient, uint256 amount) public pure override returns (bool) {
+
+  //don't know if this is needed? we could take the virtual part
+  function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+    _transfer(msg.sender, recipient, amount);
     return true;
   }
+  
+  function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+    require(sender != address(0), "ERC20: transfer from the zero address");
+    require(recipient != address(0), "ERC20: transfer to the zero address");
 
-  function allowance(address owner, address spender) public pure override returns (uint256) {
+    //_beforeTokenTransfer(sender, recipient, amount); seems like a hook that will be overriden, in the future.
+
+    uint256 senderBalance = _balances[sender];
+    require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+    _balances[sender] = senderBalance - amount;
+    _balances[recipient] += amount;
+
+    emit Transfer(sender, recipient, amount);
+  }
+
+
+
+  function allowance(address owner, address spender) public view virtual override returns (uint256) {
     return 0;
   }
 
@@ -43,4 +79,5 @@ contract HalpCoin is IERC20Upgradeable, Initializable {
   function decreaseAllowance(address spender, uint256 subtractedValue) public pure virtual returns (bool) {
     return true;
   }
+
 }
