fe444f8c37e2133eb2d859ed73440ee41942bd66
mattdogg1929 <ezekiel_yeager@protonmail.com>
2021-03-30 20:19:08 -0500

    Implemented rest of the methods in accordance to ERC20Upgradeable.sol

--- contracts/HalpCoin.sol
+++ contracts/HalpCoin.sol
@@ -12,7 +12,7 @@ contract HalpCoin is IERC20Upgradeable, Initializable {
   string private _symbol;
 
   mapping (address => uint256) private _balances;
-  //mapping (address => mapping (address => uint256)) private _allowances;
+  mapping (address => mapping (address => uint256)) private _allowances;
 
   function initialize() initializer public {
     _name = 'HalpCoin';
@@ -20,19 +20,20 @@ contract HalpCoin is IERC20Upgradeable, Initializable {
     _totalSupply = 10000000;
   }
 
-  function name() public view returns (string memory) {
+  function name() external view returns (string memory) {
     return _name;
   } 
 
-  function symbol() public view returns (string memory) {
+  function symbol() external view returns (string memory) {
     return _symbol;
   }
 
   //overriding the erc20 spec
-  function totalSupply() public view override returns (uint256) {
+  function totalSupply() external view override returns (uint256) {
     return _totalSupply;
   }
 
+  //this can be changed into external, if not called internally
   function balanceOf(address account) public view virtual override returns (uint256) {
     return _balances[account];
   }
@@ -40,7 +41,7 @@ contract HalpCoin is IERC20Upgradeable, Initializable {
 
   //don't know if this is needed? we could take the virtual part
   function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-    _transfer(msg.sender, recipient, amount);
+    _transfer(msg.sender, recipient, amount); //look at why _msgSender() is used.
     return true;
   }
   
@@ -61,22 +62,44 @@ contract HalpCoin is IERC20Upgradeable, Initializable {
 
 
   function allowance(address owner, address spender) public view virtual override returns (uint256) {
-    return 0;
+    return _allowances[owner][spender];
   }
 
-  function approve(address spender, uint256 amount) public pure override returns (bool) {
+  
+  function approve(address spender, uint256 amount) public override returns (bool) {
+    _approve(msg.sender, spender, amount); //_msgSender()
+    return true;
+
+  }
+
+  function _approve(address owner, address spender, uint256 amount) internal virtual {
+    require(owner != address(0), "ERC20: approve from the zero address");
+    require(spender != address(0), "ERC20: approve to the zero address");
+
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
+
+  function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+    _transfer(sender, recipient, amount);
+
+    uint256 currentAllowance = _allowances[sender][msg.sender];
+    require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+    _approve(sender, msg.sender, currentAllowance - amount);
+
     return true;
   }
 
-  function transferFrom(address sender, address recipient, uint256 amount) public pure override returns (bool) {
+  function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+    _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
     return true;
   }
 
-  function increaseAllowance(address spender, uint256 addedValue) public pure virtual returns (bool) {
-    return true;
-  }
-
-  function decreaseAllowance(address spender, uint256 subtractedValue) public pure virtual returns (bool) {
+  function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+    uint256 currentAllowance = _allowances[msg.sender][spender];
+    require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+    _approve(msg.sender, spender, currentAllowance - subtractedValue);
     return true;
   }
 
