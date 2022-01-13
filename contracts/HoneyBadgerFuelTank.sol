1b24324c3bcd93677dc33551f204871cee754eee
lo <innovations@herasoft.com>
2022-01-13 12:57:57 -0500

    fix fuel tank

--- contracts/HoneyBadgerFuelTank.sol
+++ contracts/HoneyBadgerFuelTank.sol
@@ -8,12 +8,12 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract HoneyBadgerFuelTank is Context, Ownable, IFuelTank {
+contract HoneyBadgerTank is Context, Ownable, IFuelTank {
   IUniswapV2Router02 uniswapRouter;
 
   address uniswapRouterAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-  address public oldHoneyBadgerAddress;
   address public honeyBadgerAddress;
+  address public hbdcAddress;
 
   mapping (address => uint) public reclaimableBalances;
   uint public liquidityBalance;
@@ -21,21 +21,21 @@ contract HoneyBadgerFuelTank is Context, Ownable, IFuelTank {
   uint public reclaimGuaranteeTime;
   uint public reclaimStartTime;
 
-  constructor (address _oldHoneyBadgerAddress) {
-    oldHoneyBadgerAddress = _oldHoneyBadgerAddress;
+  constructor (address _honeyBadgerAddress) {
+    honeyBadgerAddress = _honeyBadgerAddress;
     uniswapRouter = IUniswapV2Router02(uniswapRouterAddress);
   }
 
-  function addHoneyBadgeraddress(address _honeyBadgerAddress) public onlyOwner {
-    require(honeyBadgerAddress == address(0));
-    honeyBadgerAddress = _honeyBadgerAddress;
+  function addHBDCaddress(address _hbdcAddress) public onlyOwner {
+    require(hbdcAddress == address(0));
+    hbdcAddress = _hbdcAddress;
   }
 
   bool public nozzleOpen = false;
   function openNozzle() external override {
     require(!nozzleOpen, "AlreadyOpen");
-    require(honeyBadgerAddress != address(0), "HoneyBadgerNotInitialized");
-    require(msg.sender == honeyBadgerAddress, "MustBeHoneyBadger");
+    require(hbdcAddress != address(0), "HBDCNotInitialized");
+    require(msg.sender == hbdcAddress, "MustBeHBDC");
 
     reclaimStartTime = block.timestamp + (86400 * 2);
     reclaimGuaranteeTime = block.timestamp + (86400 * 9);
@@ -44,42 +44,42 @@ contract HoneyBadgerFuelTank is Context, Ownable, IFuelTank {
   }
 
   function addTokens(address user, uint amount) external override {
-    require(honeyBadgerAddress != address(0), "HoneyBadgerNotInitialized");
-    require(msg.sender == honeyBadgerAddress, "MustBeHoneyBadger");
+    require(hbdcAddress != address(0), "HBDCNotInitialized");
+    require(msg.sender == hbdcAddress, "MustBeHBDC");
     require(!nozzleOpen, "MustBePhase1");
 
     require(amount > 100, "amountTooSmall"); 
 
     uint granule = amount / 100;
-    uint reclaimable = granule * 72;
-    uint fuel = granule * 25;
+    uint reclaimable = granule * 0;
+    uint fuel = granule * 100;
 
     liquidityBalance += fuel;
     reclaimableBalances[user] = reclaimableBalances[user] + reclaimable;
   }
 
-  function reclaimOldHoneyBadger() private {
+  function reclaimHoneyBadgers() public {
     require(nozzleOpen, "Phase1");
     require(block.timestamp >= reclaimStartTime, "Phase2");
     address sender = msg.sender;
     require(reclaimableBalances[sender] > 0, "BalanceEmpty");
 
-    IERC20(oldHoneyBadgerAddress).transfer(sender, reclaimableBalances[sender]);
+    IERC20(honeyBadgerAddress).transfer(sender, reclaimableBalances[sender]);
     reclaimableBalances[sender] = 0;
   }
 
-  function sellOldHoneyBadger(uint256 amount, uint256 amountOutMin) public onlyOwner {
+  function sellHoneyBadger(uint256 amount, uint256 amountOutMin) public onlyOwner {
     require(nozzleOpen);
     if (block.timestamp < reclaimGuaranteeTime) {
       require(amount <= liquidityBalance, "NotEnoughFuel");
       liquidityBalance -= amount;
     }
 
-    IERC20 oldHoneyBadger = IERC20(oldHoneyBadgerAddress);
-    require(oldHoneyBadger.approve(uniswapRouterAddress, amount), "Could not approve old honeybadger transfer");
+    IERC20 honeybadger = IERC20(honeyBadgerAddress);
+    require(honeybadger.approve(uniswapRouterAddress, amount), "Could not approve honeybadger transfer");
 
     address[] memory path = new address[](2);
-    path[0] = oldHoneyBadgerAddress;
+    path[0] = honeyBadgerAddress;
     path[1] = uniswapRouter.WETH();
     uniswapRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(amount, amountOutMin, path, address(this), block.timestamp);
   }
@@ -90,19 +90,19 @@ contract HoneyBadgerFuelTank is Context, Ownable, IFuelTank {
         uint deadline) public onlyOwner {
 
     require(nozzleOpen);
-    require(honeyBadgerAddress != address(0));
+    require(hbdcAddress != address(0));
 
     address wethAddress = uniswapRouter.WETH();
 
     require(IERC20(wethAddress).approve(uniswapRouterAddress, amountWETHDesired),
       "Could not approve WETH transfer");
 
-    require(IERC20(honeyBadgerAddress).approve(uniswapRouterAddress, amountHBDCDesired),
-      "Could not approve HoneyBadger transfer");
+    require(IERC20(hbdcAddress).approve(uniswapRouterAddress, amountHBDCDesired),
+      "Could not approve HBDC transfer");
 
     uniswapRouter.addLiquidity(
       uniswapRouter.WETH(),
-      honeyBadgerAddress,
+      hbdcAddress,
       amountWETHDesired,
       amountHBDCDesired,
       amountWETHMin,
